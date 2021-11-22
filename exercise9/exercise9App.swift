fb58ae6d49a6d3245d79e004fbaa83967d533548
Davison Chang <aweila@msn.com>
2021-11-22 19:29:07 +0800

    Initial Commit

---
+++ exercise9/exercise9App.swift
@@ -0,0 +1,17 @@
+//
+//  exercise9App.swift
+//  exercise9
+//
+//  Created by Davison on 2021/11/22.
+//
+
+import SwiftUI
+
+@main
+struct exercise9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
